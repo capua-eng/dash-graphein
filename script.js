733ee56310cccebece21d3e05757d8338d5fc75c
Bruno Henrique de Lima Blois <129326520+BRUNUN0@users.noreply.github.com>
2025-04-25 10:23:43 -0300

    Modal Relatório

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ document.addEventListener('DOMContentLoaded', function() {
       e.stopPropagation();
     });
   });
+
+  // Modal
+document.addEventListener('DOMContentLoaded', function() {
+  const modal = document.getElementById('modalRelatorio');
+  const closeModal = document.querySelector('.modal .close');
+
+  // Pegando todos os links do dropdown de relatórios
+  const relatorioLinks = document.querySelectorAll('.dropdown-content a');
+
+  relatorioLinks.forEach(link => {
+      link.addEventListener('click', function(e) {
+          e.preventDefault(); // Evita o comportamento padrão do link
+          modal.style.display = 'block'; // Mostra o modal
+      });
+  });
+
+  // Fechar o modal quando clicar no botão de fechar (X)
+  closeModal.addEventListener('click', function() {
+      modal.style.display = 'none';
+  });
+
+  // Fechar o modal se clicar fora do conteúdo do modal
+  window.addEventListener('click', function(e) {
+      if (e.target == modal) {
+          modal.style.display = 'none';
+      }
+  });
+});
